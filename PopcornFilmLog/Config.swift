779a22ddcaf3283efa8d455a02380402af1c0c28
selinhasan <165820203+selinhasan@users.noreply.github.com>
2026-03-08 21:57:16 +0000

    Add API key and base URL to Config

--- PopcornFilmLog/Config.swift
+++ PopcornFilmLog/Config.swift
@@ -1,6 +1,5 @@
 import Foundation
-
 nonisolated enum Config {
-    static let EXPO_PUBLIC_TMDB_API_KEY = ""
-    static let EXPO_PUBLIC_RORK_API_BASE_URL = ""
+    static let EXPO_PUBLIC_TMDB_API_KEY = "sb_publishable_djMoK2JmZB3tT7J6lXisnA_L6cLmoFx"
+    static let EXPO_PUBLIC_RORK_API_BASE_URL = "https://rork-popcorn-film-log.vercel.app"
 }
