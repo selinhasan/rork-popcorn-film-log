bb997a59eba559316cfdba5376042c1dd67cebea
selinhasan <165820203+selinhasan@users.noreply.github.com>
2026-03-08 22:01:00 +0000

    Update TMDB API key in Config.swift

--- PopcornFilmLog/Config.swift
+++ PopcornFilmLog/Config.swift
@@ -1,5 +1,5 @@
 import Foundation
 nonisolated enum Config {
-    static let EXPO_PUBLIC_TMDB_API_KEY = "sb_publishable_djMoK2JmZB3tT7J6lXisnA_L6cLmoFx"
+    static let EXPO_PUBLIC_TMDB_API_KEY = "75aedc36f43a3f33099c488a2e443585"
     static let EXPO_PUBLIC_RORK_API_BASE_URL = "https://rork-popcorn-film-log.vercel.app"
 }
