2e374e156fd06669a67f6b023eb3ac5a1ccf4238
selinhasan <165820203+selinhasan@users.noreply.github.com>
2026-03-13 14:15:36 +0000

    edit stack navigator

--- App.jsx
+++ App.jsx
@@ -7,8 +7,8 @@ import { AuthProvider, useAuth } from './src/context/AuthContext'
 import { AppProvider } from './src/context/AppContext'
 import { Colors } from './src/theme/colors'
 
-import LoginScreen from './src/screens/LoginScreen'
 import RegisterScreen from './src/screens/RegisterScreen'
+import LoginScreen from './src/screens/LoginScreen'
 import DiaryScreen from './src/screens/DiaryScreen'
 import BrowseScreen from './src/screens/BrowseScreen'
 import BuddiesScreen from './src/screens/BuddiesScreen'
@@ -82,15 +82,15 @@ function RootNavigator() {
         <>
           <Stack.Screen name="Main" component={MainTabs} />
           <Stack.Screen
-            name="LogFilm"
-            component={LogFilmScreen}
+            name="Buddies"
+            component={BuddiesScreen}
             options={{ presentation: 'modal' }}
           />
         </>
       ) : (
         <>
-          <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
         </>
       )}
     </Stack.Navigator>
